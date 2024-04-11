086963406a5823ca8cec82f7a41f82ceaad2f89f
Wilfred Lemayian <wilfred.lemayian@gmail.com>
2024-04-11 23:01:53 +0300

    New APi

--- index.js
+++ index.js
@@ -1,28 +1,78 @@
-function fetchWeatherData() {
-    const locationInput = document.getElementById('location-value').value.trim();
-    const weatherLocation = document.getElementById('location');
-    const temperature = document.getElementById('temperature');
-    const description = document.getElementById('description');
-    const API_URL = "https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0";
+window.addEventListener('load', function(){
+    const CORS_PROXY = `https://corsproxy.io/?`;
+    const BASE_API_URL = `${CORS_PROXY}https://api.open-meteo.com/v1`;
+    const GEOCODING_API_URL = `${CORS_PROXY}https://geocoding-api.open-meteo.com/v1`;
+    let forecastData = [];
 
-    if (locationInput === '') {
-        alert('Please enter location');
-        return;
+    function fetchWeatherData() {
+        const locationInput = document.getElementById('location-value').value.trim();
+        const weatherLocation = document.getElementById('location');
+        const temperature = document.getElementById('temperature');
+        const description = document.getElementById('description');
+        const forecastList = document.getElementById('forecasts-list');
+
+        if (locationInput === '') {
+            alert('Please enter location');
+            return;
+        }
+
+        fetch(`${GEOCODING_API_URL}/search?name=${encodeURIComponent(locationInput)}&count=1&language=en&format=json`)
+        .then(response => response.json())
+        .then(location => {
+
+            // get longitude and latitude
+            const longitude = location.results[0].longitude;
+            const latitude = location.results[0].latitude;
+            const timezone = location.results[0].timezone;
+
+            // update location in HTML
+            document.querySelector("#location").innerHTML = location.results[0].name + ", " + location.results[0].country;
+            
+            
+            // fetch weather data using geocoded location
+            fetch(`${BASE_API_URL}/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&hourly=temperature_2m`)
+            .then(response => response.json())
+            .then(data => {
+                forecastList.innerHTML = ``;
+                
+                // clear forecasts array
+                forecastData = [];
+
+                let html = ``;
+
+                // save data to forecast array
+                for(let i = 0; i < data.hourly.time.length; i++){
+                    forecastData.push({
+                        time: new Date(data.hourly.time[i]),
+                        temp: data.hourly.temperature_2m[i]
+                    });
+                }
+
+                // map the data to html
+                forecastData.map(data => {
+                    html += `
+                        <div>
+                            <span>${formatDate(data.time)}</span>
+                            <span><strong>${data.temp}&deg;C</strong></span>
+                        </div>
+                    `;
+                });
+
+            forecastList.innerHTML = html;
+
+
+
+            })
+            .catch(error =>  console.log(error))
+
+        })
+        .catch(error =>  console.log(error))
     }
 
-    fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {
-            const weatherData = data.dataseries[1];
-            console.log(weatherData);
-            console.log(weatherData.seeing);
-            weatherLocation.textContent = "Location: " + locationInput;
-            temperature.textContent = `${"Temperature: " + weatherData.temp2m}°C`;
-            description.textContent = "Description: " + weatherData.prec_type;
-        })
-        .catch(error => console.log(error));
-}
+  
+    // Event listener for button click
+    document.getElementById('btn').addEventListener('click', fetchWeatherData);
 
-// Event listener for button click
-document.getElementById('btn').addEventListener('click', fetchWeatherData);
+    
 
+});
