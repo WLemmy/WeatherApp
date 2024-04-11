9335482167b6382f72177fdd2b014aae2a72508e
Wilfred Lemayian <wilfred.lemayian@gmail.com>
2024-04-11 23:03:10 +0300

    Format Date

--- index.js
+++ index.js
@@ -69,7 +69,21 @@ window.addEventListener('load', function(){
         .catch(error =>  console.log(error))
     }
 
-  
+        // format date
+        function formatDate(date){
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+
+            if(hours < 10){
+                hours = "0" + hours;
+            }
+
+            if(minutes < 10){
+                minutes = "0" + minutes;
+            }
+
+            return ${hours}:${minutes};
+}
     // Event listener for button click
     document.getElementById('btn').addEventListener('click', fetchWeatherData);
 
